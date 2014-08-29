ed821540418015a522196f632965c7be3ee5b5fe
georgeebbs <gebbsking@gmail.com>
2014-08-29 04:39:41 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////
+//George Ebbs
+//Lab One
+// Hello World Java Program
+//
+//  First compile the program
+//      javac HelloWorld.java
+//  run the program
+//      jave HelloWorld//
+
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[ ] args) {
+        
+//  print the statement with box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2014!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest
+    }
+}
